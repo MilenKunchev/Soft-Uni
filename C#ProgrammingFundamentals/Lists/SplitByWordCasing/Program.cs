d7392f68921abf984c9ff8540cdbebe34f4f5e61
Milen <Milen_kunchev@abv.bg>
2017-10-26 18:31:10 +0300

    Add more exercises

---
+++ C#ProgrammingFundamentals/Lists/SplitByWordCasing/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] separators = { ',', ';', ':', '.', '!', '(', ')', '"', '\'', '\\', '/', '[', ']', ' ' };
+            List<string> text = Console.ReadLine().Split(separators, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            List<string> lowerCaseWords = new List<string>();
+            List<string> upperCaseWords = new List<string>();
+            List<string> mixedCaseWords = new List<string>();
+
+            foreach (var item in text)
+            {
+                string word = item;
+                bool allIsUppercase = true;
+                bool allislowercase = true;
+
+                for (int i = 0; i < word.Length; i++)
+                {
+                    char character = word[i];
+
+                    if (char.IsUpper(character))
+                    {
+                        allislowercase = false;
+                    }
+                    else if (char.IsLower(character))
+                    {
+                        allIsUppercase = false;
+                    }
+                    else
+                    {
+                        allislowercase = false;
+                        allIsUppercase = false;
+                    }
+                }
+                if (allIsUppercase)
+                {
+                    upperCaseWords.Add(word);
+                }
+                else if (allislowercase)
+                {
+                    lowerCaseWords.Add(word);
+                }
+                else
+                {
+                    mixedCaseWords.Add(word);
+                }
+            }
+            Console.WriteLine("Lower-case: " + string.Join(", ", lowerCaseWords));
+            Console.WriteLine("Mixed-case: " + string.Join(", ", mixedCaseWords));
+            Console.WriteLine("Upper-case: " + string.Join(", ", upperCaseWords));
+        }
+    }
+}
---
+++ C#ProgrammingFundamentals/Lists/SquareNumbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            
+            var squareNumbers = ReturnSquareNumbers(numbers);
+            var sortedNumbers = SortNumbers(squareNumbers);
+            
+            Console.WriteLine(string.Join(" ", sortedNumbers));
+        }
+
+        private static List<int> SortNumbers(List<int> squareNumbers)
+        {            
+            squareNumbers.Sort((a, b) => b.CompareTo(a));
+            return squareNumbers;
+        }
+
+        private static List<int> ReturnSquareNumbers(List<int> numbers)
+        {
+            List<int> squareNumbers = new List<int>();
+            foreach (var item in numbers)
+            {
+                bool isItSquare = (Math.Sqrt(item)) == ((int)Math.Sqrt(item));
+                if (isItSquare)
+                {
+                    squareNumbers.Add(item);
+                }
+            }
+            return squareNumbers;
+        }
+    }
+}
